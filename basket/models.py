7dd5a9db7bbafbd7b0a454ac05b784ed79c0c736
Anton K <ihatelags@gmail.com>
2021-02-06 12:24:54 +0300

    Homework 5

---
+++ basket/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from authapp.models import User
+from mainapp.models import Product
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт {self.product.name}'
+
+    def sum(self):
+        return self.quantity * self.product.price
+
+    def total_basket(self):
+        return Basket.objects.filter(user=self.user)
+
+    def total_sum(self):
+        return sum([basket.sum() for basket in self.total_basket()])
+
+    def total_qty(self):
+        return sum([basket.quantity for basket in self.total_basket()])
